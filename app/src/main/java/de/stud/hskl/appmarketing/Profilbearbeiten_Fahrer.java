e8322ec9e934875470e3fc1b1c752201cb83b21b
chst0043 <chst0043@stud.hs-kl.de>
2022-12-14 18:28:17 +0100

    * Views angelget (code in klassen fehlt noch) * code für chat entickelt (unfertig)

---
+++ app/src/main/java/de/stud/hskl/appmarketing/Profilbearbeiten_Fahrer.java
@@ -0,0 +1,18 @@
+package de.stud.hskl.appmarketing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class Profilbearbeiten_Fahrer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilbearbeiten_fahrer);
+
+    }
+
+}
