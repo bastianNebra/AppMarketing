af39669cec57c80901c75e59353cfdcfa80dcebd
echo <elei1001@stud.hs-kl.de>
2022-12-15 01:34:03 +0100

    Dasboard; spezifische Änderungen in den Java-Klassen bitte Kommentare beachten; in CardView fahrt_card_item_list.xmltext entfernen um Daten anzuzeigen

---
+++ app/src/main/java/de/stud/hskl/appmarketing/DriveData.java
@@ -0,0 +1,15 @@
+package de.stud.hskl.appmarketing;
+/*
+muss im verlauf je nachdem wie ihr die daten in der Datenbank habt geändert werden
+alle private
+zu jedem Getter-/Setter
+Konstruktor mit Parameterliste
+ */
+public class DriveData {
+    private String Ziel;
+    private String Start;
+    //abfahrt zeit
+    //ankunft zeit
+    //anzahl mitfahrer
+    //...
+}
